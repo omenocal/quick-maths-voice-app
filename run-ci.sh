ac763a48be703f99e03ee6899cdfa64bfb6cd639
omenocal <octavio_menocal92@hotmail.es>
2019-07-29 13:36:55 -0600

    Removes check-coverage validation

--- run-ci.sh
+++ run-ci.sh
@@ -6,7 +6,7 @@ mkdir .nyc_output
 yarn run test-ci
 yarn run report
 yarn run lint
-npx nyc check-coverage
+#npx nyc check-coverage
 
 if [ "${CI:-}" = "true" ]; then
   cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js
